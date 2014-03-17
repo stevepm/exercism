e0c7e5e9e4116ba050b4060fb0c3f7b753b73504
stevepm <steven.magelowitz@gmail.com>
2014-03-17 09:53:52 -0600

    gigasecond is passing in the correct new date

--- ruby/gigasecond/gigasecond_test.rb
+++ ruby/gigasecond/gigasecond_test.rb
@@ -11,22 +11,19 @@ class GigasecondTest < MiniTest::Unit::TestCase
   end
 
   def test_2
-    skip
     gs = Gigasecond.new(Date.new(1977, 6, 13))
     assert_equal Date.new(2009, 2, 19), gs.date
   end
 
   def test_3
-    skip
     gs = Gigasecond.new(Date.new(1959, 7, 19))
     assert_equal Date.new(1991, 3, 27), gs.date
   end
 
-  def test_yourself
-    skip
-    your_birthday = Date.new(year, month, day)
-    gs = Gigasecond.new(your_birthday)
-    assert_equal Date.new(2009, 1, 31), gs.date
-  end
+  #def test_yourself
+  #  your_birthday = Date.new(1988, 9, 15)
+  #  gs = Gigasecond.new(your_birthday)
+  #  assert_equal Date.new(2009, 1, 31), gs.date
+  #end
 
 end
